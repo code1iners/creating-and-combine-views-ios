7101a6eadb82d4d9f5b16b2adaf8a28cf6ecff0c
Codeliner <codeliner96@gmail.com>
2021-03-15 16:54:24 +0900

    Createing and Combine views done

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Ethan on 2021/03/14.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.543_184, longitude: 127.075_145),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
